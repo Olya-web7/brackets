a58998af65c695aad0789d0eb2d1558679a313fe
Olya <shyshova24@gmail.com>
2021-07-28 18:44:26 +0300

    brackrts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let pairs = []
+	let result = undefined
+	for (let i = 0; i < bracketsConfig.length; i++) {
+		pairs[i] = bracketsConfig[i].join('')
+	}
+
+	for (let pair of pairs) {
+		if (result === undefined) {
+			result = str.split(pair).join('')
+		} else {
+			result = result.split(pair).join('')
+		}
+	}
+	if (str === '') return true
+	if (str === result) return false
+	if (str !== result) return check(result, bracketsConfig)
 }
